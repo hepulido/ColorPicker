5466ef709a08a72b0f450b3c3491f91c5e741dd4
hector <hpulido1021@gmail.com>
2023-06-22 20:43:50 -0400

    Feature: fixed color component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-// Write your Color component here
+import { useState } from "react";
+import "./index.css"
+
+const Color = ({ color, selectedColor, setSelectedColor }) => {
+  console.log("setSelectedColor", setSelectedColor)
+  const isSelected = selectedColor === color;
+  return (
+    <div
+      className={`${color} ${isSelected ? "selected" : ""}`}
+      onClick={() => setSelectedColor(color)}
+    ></div>
+  );
+};
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("");
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color color="red" setSelectedColor={setSelectedColor} selectedColor ={selectedColor} />
+        <Color color="blue" setSelectedColor={setSelectedColor} selectedColor ={selectedColor} />
+        <Color color="green" setSelectedColor={setSelectedColor} selectedColor ={selectedColor} />
+      </div>
     </div>
   );
 };
